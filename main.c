2fc83e1c4a5fe66f64ab1469609e260bc5065f3b
Your Name <you@example.com>
2019-01-17 09:13:42 +0800

    添加一个敏感字符

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+int index = 0;
+char arr[1024];
+
+int isMingan(char x)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
+    {
+        if (x == arr[i])
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 
 int main()
 
@@ -13,10 +35,23 @@ int main()
         printf("3----chakansuoyouminganzifu\n");
         printf("4----tihuan\n");
         printf("5----tuichu\n");
-
+        arr[index] = 0;
+        index++;
         printf("xuanzexiangyinggongneng\n");
         scanf("%d", &coude);
     }
+    if (coude == 1)
+        {
+            char a;
+            printf("qingshuruyaotianjiademinganzifu\n");
+            scanf("%c", &a);
+            scanf("%c", &a);
+            printf("tianjiachenggong,dianjihuichejixu\n");
+
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
     return 0;
 
 }
