ddd8960efab064ce0ea7d04082d6c47c32ecd6ff
Your Name <you@example.com>
2019-01-17 08:58:51 +0800

    构建基本结构框架

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-int main(){
+int main()
 
-    printf("hello world!");
+{
 
+    int coude = 0;
+    printf("guanliminganzifuxitong\n");
+    while (1)
+    {
+        printf("1----tianjia\n");
+        printf("2----shanchuzuihouyigeminganzifu\n");
+        printf("3----chakansuoyouminganzifu\n");
+        printf("4----tihuan\n");
+        printf("5----tuichu\n");
+
+        printf("xuanzexiangyinggongneng\n");
+        scanf("%d", &coude);
+    }
     return 0;
 
 }
